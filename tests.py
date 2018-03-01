4da1d186c09d0cef0f08b09d916bcdfa872b9564
james947 <jmuriuki947@gmail.com>
2018-03-01 01:22:10 -0800

    Add comments

--- tests.py
+++ tests.py
@@ -2,34 +2,31 @@ import unittest
 from phone import Phone
 
 class PhoneTestCase(unittest.TestCase):
-    #tests if number is added 
+#tests if number is added 
     def test_add_new_number(self):
         new = Phone()
         response=new.add_new(name="james",number=555)
         self.assertEqual(response['message'], "Business added succesfully")
-
+#removes contacts from list
     def test_delete_contact(self):
         new = Phone()
         response=new.add_new(name="james",number=555)
         response=new.delete(name="james")
         self.assertEqual(response['message'], "Contact deleted succesfully")
-
+#returns all contacts
     def test_view_contacts(self):
         new=Phone()
         response=new.add_new(name="james",number=555)
         response=new.view_contacts()
         self.assertEqual(response,{'number': 555, 'name': 'james'})
 
+#updates contacts
     def test_update_contacts(self):
         new=Phone()
         response=new.add_new(name="james",number=555)
         response=new.update_contacts(name="ken",number=222)
         self.assertEqual(response['message'], "Contact updated succesfully")
         
-
-
-
-
-
+#starts the tests
 if __name__ == '__main__':
     unittest.main()
